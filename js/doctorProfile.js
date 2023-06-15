2d851569a7a8100f40c87066eea25a83244eeaba
mohamed adel <Mibo82239@gmail.com>
2023-06-15 20:35:28 +0300

    a

--- js/doctorProfile.js
+++ js/doctorProfile.js
@@ -1,44 +1,4 @@
-const allListProfiles = document.querySelectorAll(
-  ".allInfo .text-list .lists ul li"
-);
 
-document.getElementById("birth").addEventListener("click", () => {
-  remove();
-  document.getElementById("birth").classList.add("active");
-  document.querySelector(".info").classList.add("right");
-});
-
-document.getElementById("general").addEventListener("click", () => {
-  remove();
-  document.getElementById("general").classList.add("active");
-  document.querySelector(".about").classList.add("right");
-});
-
-document.getElementById("over").addEventListener("click", () => {
-  remove();
-  document.getElementById("over").classList.add("active");
-  document.querySelector(".overview").classList.add("right");
-});
-function remove() {
-  allListProfiles.forEach((ele) => {
-    ele.classList.remove("active");
-  });
-  document.querySelectorAll(".allInfo .text-list .text div").forEach((ele) => {
-    ele.classList.remove("right");
-  });
-}
-
-/*
-    -- log out 
-*/
-
-document.querySelector("#login-btn1").addEventListener("click", () => {
-  window.location.href = "../index.html";
-});
-document.querySelector("#login-btn").addEventListener("click", () => {
-  window.location.href = "../index.html";
-
-});
 
 /*
     -- event handlers to return hte first page
@@ -174,4 +134,46 @@ document
 document.getElementById("delete").addEventListener("click", () => {
   localStorage.setItem("name-1", "");
   window.location.reload();
+});
+
+const allListProfiles = document.querySelectorAll(
+  ".allInfo .text-list .lists ul li"
+);
+
+document.getElementById("birth").addEventListener("click", () => {
+  remove();
+  document.getElementById("birth").classList.add("active");
+  document.querySelector(".info").classList.add("right");
+});
+
+document.getElementById("general").addEventListener("click", () => {
+  remove();
+  document.getElementById("general").classList.add("active");
+  document.querySelector(".about").classList.add("right");
+});
+
+document.getElementById("over").addEventListener("click", () => {
+  remove();
+  document.getElementById("over").classList.add("active");
+  document.querySelector(".overview").classList.add("right");
+});
+function remove() {
+  allListProfiles.forEach((ele) => {
+    ele.classList.remove("active");
+  });
+  document.querySelectorAll(".allInfo .text-list .text div").forEach((ele) => {
+    ele.classList.remove("right");
+  });
+}
+
+/*
+    -- log out 
+*/
+
+document.querySelector("#login-btn1").addEventListener("click", () => {
+  window.location.href = "../index.html";
+});
+document.querySelector("#login-btn").addEventListener("click", () => {
+  window.location.href = "../index.html";
+
 });
