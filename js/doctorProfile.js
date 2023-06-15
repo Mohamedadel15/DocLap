b4a52dcfba19cdfff13007b122183a8df99fb89c
mohamed adel <Mibo82239@gmail.com>
2023-06-15 21:12:03 +0300

    a

--- js/doctorProfile.js
+++ js/doctorProfile.js
@@ -79,36 +79,6 @@ document.getElementById("delete").addEventListener("click", () => {
   window.location.reload();
 });
 
-const allListProfiles = document.querySelectorAll(
-  ".allInfo .text-list .lists ul li"
-);
-
-document.getElementById("birth").addEventListener("click", () => {
-  remove();
-  document.getElementById("birth").classList.add("active");
-  document.querySelector(".info").classList.add("right");
-});
-
-document.getElementById("general").addEventListener("click", () => {
-  remove();
-  document.getElementById("general").classList.add("active");
-  document.querySelector(".about").classList.add("right");
-});
-
-document.getElementById("over").addEventListener("click", () => {
-  remove();
-  document.getElementById("over").classList.add("active");
-  document.querySelector(".overview").classList.add("right");
-});
-function remove() {
-  allListProfiles.forEach((ele) => {
-    ele.classList.remove("active");
-  });
-  document.querySelectorAll(".allInfo .text-list .text div").forEach((ele) => {
-    ele.classList.remove("right");
-  });
-}
-
 
 
 /*
